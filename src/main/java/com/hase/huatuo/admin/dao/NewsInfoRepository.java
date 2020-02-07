e84697686298dee235422bf728764720ee00a186
shenpeng <shenpcn@foxmail.com>
2020-02-07 23:36:34 +0800

    fixed delete news-info bug

--- src/main/java/com/hase/huatuo/admin/dao/NewsInfoRepository.java
+++ src/main/java/com/hase/huatuo/admin/dao/NewsInfoRepository.java
@@ -3,8 +3,10 @@ package com.hase.huatuo.admin.dao;
 
 import com.hase.huatuo.admin.dao.entity.NewsInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,4 +15,9 @@ public interface NewsInfoRepository extends JpaRepository<NewsInfo, String> {
     NewsInfo getNewsInfoByIdEquals(Long  newsId);
     @Query(nativeQuery = true,value = "SELECT ni.* FROM news_info ni WHERE ni.enable='Y' AND ni.priority=1 ORDER BY ni.date DESC LIMIT 0,5")
     List<NewsInfo> getImportantNewsList();
+
+    @Transactional
+    @Modifying
+    @Query("delete from NewsInfo where id = ?1")
+    void deleteById(Long s);
 }
